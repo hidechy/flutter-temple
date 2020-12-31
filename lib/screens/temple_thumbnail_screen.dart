b4c78b20a0d0b67014b13b8ff3f717b203fb7a26
Hide Toyoda <hide.toyoda@gmail.com>
2020-12-31 20:48:41 +0900

    gohonzon display

--- lib/screens/temple_thumbnail_screen.dart
+++ lib/screens/temple_thumbnail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart';
@@ -121,6 +122,7 @@ class _TempleThumbnailScreenState extends State<TempleThumbnailScreen> {
                         Padding(
                           padding: const EdgeInsets.all(10.0),
                           child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: <Widget>[
                               Text('${widget.data['temple']}'),
                               Text('${widget.data['gohonzon']}')
